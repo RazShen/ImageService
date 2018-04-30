224bf887260af54fd6c2c5adfd24032824a983f3
raz Shenkman <shenkmanraz@gmail.com>
2018-04-30 17:03:24 +0300

    client

---
+++ ImageServiceGUI/Client/IClientHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI.Client
+	{
+	public interface IClientHandler
+		{
+		void HandleClient(TcpClient client);
+		}
+	}
