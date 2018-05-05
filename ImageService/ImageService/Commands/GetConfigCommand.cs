a3ea11a8298272f2a55884a9e2f5aa0ff4a83b12
Tomer Grady <tomergrady@gmail.com>
2018-05-05 14:40:15 +0300

    comand

--- ImageService/ImageService/Commands/GetConfigCommand.cs
+++ ImageService/ImageService/Commands/GetConfigCommand.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿
+using ImageService.Modal;
+using Newtonsoft.Json;
+using SharedFiles;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ImageService.Commands
-	{
-	class GetConfigCommand
-		{
-		}
-	}
+{
+    class GetConfigCommand : ICommand
+    {
+        static class Constants
+        {
+            public const int size = 5;
+        }
+
+        public string Execute(string[] args, out bool result)
+        {
+            try
+            {
+                result = true;
+                string[] arr = new string[Constants.size];
+                arr[0] = ConfigurationManager.AppSettings.Get("OutputDir");
+                arr[1] = ConfigurationManager.AppSettings.Get("SourceName");
+                arr[2] = ConfigurationManager.AppSettings.Get("LogName");
+                arr[3] = ConfigurationManager.AppSettings.Get("ThumbnailSize");
+                arr[4] = ConfigurationManager.AppSettings.Get("Handler");
+                CommandRecievedEventArgs commandSendArgs = new CommandRecievedEventArgs((int)CommandEnum.GetConfigCommand, arr, "");
+                return JsonConvert.SerializeObject(commandSendArgs);
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                return ex.ToString();
+            }
+        }
+    }
+}
