ebe934028241275ce9c68d9aacd049b802619a7b
raz Shenkman <shenkmanraz@gmail.com>
2018-06-02 22:11:16 +0300

    commit

--- ImageServiceWebApplication/App_Start/RouteConfig.cs
+++ ImageServiceWebApplication/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace ImageServiceWebApplication.AppStart
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+			// 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
