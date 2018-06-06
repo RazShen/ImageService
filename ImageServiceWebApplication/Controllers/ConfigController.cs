e908fe1632f3df837acefb1b4ec5869043fd2657
raz Shenkman <shenkmanraz@gmail.com>
2018-06-06 15:34:11 +0300

    update tomer files

--- ImageServiceWebApplication/Controllers/ConfigController.cs
+++ ImageServiceWebApplication/Controllers/ConfigController.cs
@@ -1,21 +1,42 @@
 ﻿
 using ImageServiceWebApplication.Models;
-using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace ImageServiceWebApplication.Controllers
-{
-    public class ConfigController : Controller
-    {
-        static ConfigModel cm = new ConfigModel();
-        // GET: Config
-        public ActionResult Config()
-        {
+	{
+	public class ConfigController : Controller
+		{
+		public static ConfigModel cm = new ConfigModel();
+		private static string handlerToDelete;
 
-            return View(cm);
-        }
-    }
-}
+		// GET: Config
+		public ActionResult Config()
+			{
+
+			return View(cm);
+			}
+
+		public ActionResult DeleteHandler(string handler)
+			{
+			handlerToDelete = handler;
+			return View("Confirmation");
+
+			}
+
+		public ActionResult DeleteOK()
+			{
+			cm.DeleteHandler(handlerToDelete);
+			System.Threading.Thread.Sleep(1000);
+			return RedirectToAction("Config");
+			}
+
+		public ActionResult DeleteCancel()
+			{
+			return RedirectToAction("Config");
+			}
+		}
+	}
--- ImageServiceWebApplication/Models/ConfigModel.cs
+++ ImageServiceWebApplication/Models/ConfigModel.cs
@@ -11,84 +11,93 @@ namespace ImageServiceWebApplication.Models
 	public class ConfigModel
 		{
 
-        //members
-        private Client.Configurations configurations;
-        private IClient ImageWebModelClient { get; set; }
+		//members
+		private Client.Configurations configurations;
+		private IClient ImageWebModelClient { get; set; }
 
 
-        public ConfigModel()
-        {
-            //  configurations = Client.Configurations.Instance;
-            //    SourceName = configurations.SourceName;
-            //      LogName = configurations.LogName;
-            //        OutputDirectory = configurations.OutputDirectory;
-            //          Handlers = configurations.Handlers;
-            //            ThumbnailSize = configurations.TumbnailSize;
-            this.ImageWebModelClient = Client.Client.Instance;
+		public ConfigModel()
+			{
+			//  configurations = Client.Configurations.Instance;
+			//    SourceName = configurations.SourceName;
+			//      LogName = configurations.LogName;
+			//        OutputDirectory = configurations.OutputDirectory;
+			//          Handlers = configurations.Handlers;
+			//            ThumbnailSize = configurations.TumbnailSize;
+			this.ImageWebModelClient = Client.Client.Instance;
+			this.ImageWebModelClient.UpdateEvent += ConstUpdate;
+			SourceName = "ss";
+			LogName = "ee";
+			OutputDirectory = "we";
+			ThumbnailSize = "size";
 
-            SourceName = "ss";
-            LogName = "ee";
-            OutputDirectory = "we";
-            ThumbnailSize = "size";
+			List<string> Handlers2 = new List<string>
+			{
+				"11", "22"
+			};
+			Handlers = Handlers2;
+			}
 
-            List<string> Handlers2 = new List<string>
-            {
-                "11", "22"
-            };
-            Handlers = Handlers2;
-        }
+		public bool DeleteHandler(string HandlerToDelete)
+			{
+			try
+				{
+				string[] arr = { HandlerToDelete };
+				if (!ImageWebModelClient.Running()) { return false; }
+				CommandRecievedEventArgs commandReq = new CommandRecievedEventArgs((int)CommandEnum.CloseHandlerCommand, arr, "");
+				this.ImageWebModelClient.WriteCommandToServer(commandReq);
+				}
+			catch (Exception e)
+				{
+				Console.WriteLine(e.ToString());
+				return false;
+				}
+			return true;
+			}
 
-        private bool DeleteHandler(string HandlerToDelete)
-        {
-            try
-            {
-                string[] arr = { HandlerToDelete };
-                if (!ImageWebModelClient.Running()) { return false; }
-                CommandRecievedEventArgs commandReq = new CommandRecievedEventArgs((int)CommandEnum.CloseHandlerCommand, arr, "");
-                this.ImageWebModelClient.WriteCommandToServer(commandReq);
-                //this.ImageWebModelClient.
+		private void CloseHandler(CommandRecievedEventArgs responseObj)
+			{
+			if (Handlers.Contains(responseObj.Args[0]) && responseObj.Args != null
+				&& responseObj != null && Handlers != null)
+				{
+				this.Handlers.Remove(responseObj.Args[0]);
+				}
+			}
+		private void ConstUpdate(CommandRecievedEventArgs args)
+			{
+			if (args != null)
+				{
+				switch (args.CommandID)
+					{
+					case (int)CommandEnum.CloseHandlerCommand:
+						CloseHandler(args);
+						break;
+					}
+				}
+			}
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Output Directory:  ")]
+		public string OutputDirectory { get; set; }
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return false;
-            }
-            return true;
-        }
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Source Name:   ")]
+		public string SourceName { get; set; }
 
-        private void CloseHandler(CommandRecievedEventArgs responseObj)
-        {
-            if (Handlers.Contains(responseObj.Args[0]) && responseObj.Args != null
-                && responseObj != null && Handlers != null)
-            {
-                this.Handlers.Remove(responseObj.Args[0]);
-            }
-        }
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Log Name:  ")]
+		public string LogName { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Output Directory:  ")]
-        public string OutputDirectory { get; set; }
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Thumbnail Size:    ")]
+		public string ThumbnailSize { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Source Name:   ")]
-        public string SourceName { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Log Name:  ")]
-        public string LogName { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Thumbnail Size:    ")]
-        public string ThumbnailSize { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Handlers: ")]
-        public List<string> Handlers { get; set; }
-    }
-}
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Handlers: ")]
+		public List<string> Handlers { get; set; }
+		}
+	}
